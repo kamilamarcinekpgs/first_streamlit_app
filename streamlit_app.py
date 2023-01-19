8ed7f5de147a93403a13049898d8c59b62861406
kamilamarcinekpgs <87711035+kamilamarcinekpgs@users.noreply.github.com>
2023-01-19 19:51:11 +0100

    Update streamlit_app.py
    reorganized code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import requests
 import snowflake.connector
+import pandas
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -12,7 +14,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
